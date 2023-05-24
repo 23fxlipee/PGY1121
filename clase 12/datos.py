8ba96b446b79956bfdabdc274f2757cd807de035
23fxlipee <134543098+23fxlipee@users.noreply.github.com>
2023-05-24 14:52:16 -0400

    datos validados

--- clase 12/datos.py
+++ clase 12/datos.py
@@ -3,9 +3,16 @@
 print("datos personales")
 print("-----------------")
 vNom = input("ingrese su nombre: ")
-vedad = input("ingrese su edad: ")
+while True:
+    try :
+     vEdad = int(input("ingrese su edad: "))
+     break
+
+    except:
+        print("valor no corresponde")
+
 print("-------------------")
 print(f"su nombre es: {vNom} ")
-print(f"su edad es: {vedad} ")
+print(f"su edad es:   {vEdad} ")
 
 print("Programa finalizado....")
