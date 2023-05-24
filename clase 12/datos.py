cfda7b6709be1fa1ba165382c66f6e6800724d32
23fxlipee <134543098+23fxlipee@users.noreply.github.com>
2023-05-24 14:33:39 -0400

    Archivo dato.py sin validar

---
+++ clase 12/datos.py
@@ -0,0 +1,11 @@
+#EJEMPLO DE USO DE REPOSITORIO
+
+print("datos personales")
+print("-----------------")
+vNom = input("ingrese su nombre: ")
+vedad = input("ingrese su edad: ")
+print("-------------------")
+print(f"su nombre es: {vNom} ")
+print(f"su edad es: {vedad} ")
+
+print("Programa finalizado....")
